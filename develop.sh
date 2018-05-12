b3a7720dcef6a12a146ca5b18373e24c4a5c9b2e
skyline75489 <skyline75489@outlook.com>
2018-05-12 11:13:50 +0800

    update 2018年 5月12日 星期六 11时13分50秒 CST

--- develop.sh
+++ develop.sh
@@ -6,7 +6,7 @@ git init
 git remote add origin https://github.com/skyline75489/Heart-First-JavaWeb.git
 git fetch
 git checkout origin/gh-pages -b gh-pages
-cd ../interview
+cd ../Heart-First-JavaWeb
 cd source
 gitbook install
 cd ..
